dacb1b4971b39af186af1d3c3a3238128cd53217
EvansMutwiri <mutwirievansm@gmail.com>
2022-01-02 23:39:15 +0300

    Error handling

--- ArrayLinearSearch.java
+++ ArrayLinearSearch.java
@@ -1,14 +1,15 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArrayLinearSearch {
     public static void main(String[] args) {
         try (Scanner s = new Scanner(System.in)) {
-            int[] orderNumbers = {12345, 54321, 23411, 101010, 456789, 567895 };
+            int[] orderNumbers = { 12345, 54321, 23411, 101010, 456789, 567895 };
 
             int toFind;
 
             //total number of orders
-            System.out.println(orderNumbers.length);
+            System.out.println("TOTAL ORDERS: "+orderNumbers.length);
 
             //print each order
             for ( int order : orderNumbers) {
@@ -21,13 +22,14 @@ public class ArrayLinearSearch {
             try {
                 toFind = s.nextInt();
                 for ( int order : orderNumbers) {
-                    if (order == toFind) {
-                        System.out.println( order + " found");
+                    boolean found = order == toFind;
+                    if (found) {
+                        System.out.println( "Order number: " + order + " found");
                     }
                 }
 
-            } catch (Exception e) {
-                System.out.println("Invalid input");
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input -- must be a number");
             }
         }
         
